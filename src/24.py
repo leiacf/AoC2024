2782199dcedade30b5246d76e96e860f646e601e
Nina Høegh-Larsen <ninius@online.no>
2024-11-30 11:40:24 +0100

    Adding tools and init of days

---
+++ src/24.py
@@ -0,0 +1,38 @@
+#Advent of Code 2024 Day 24
+
+from tools import files
+import time
+
+def test():
+
+    input = [
+
+    ]
+
+    return input
+
+def part1(input):
+
+    print("Part 1: ")
+
+def part2(input):
+
+    print("Part 2: ")
+
+filename = "../input/24.txt"
+input = files.input_as_list(filename)
+#input = test()
+
+print()
+
+start1 = time.perf_counter()
+part1(input)
+end1 = time.perf_counter()
+
+start2 = time.perf_counter()
+part2(input)
+end2 = time.perf_counter()
+
+print()
+print("Spent {:>7.2f} seconds on Part 1".format(end1-start1))
+print("Spent {:>7.2f} seconds on Part 2".format(end2-start2))
