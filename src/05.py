36e5489d9320a557273f732b9e549c7b4e38168f
Nina Høegh-Larsen <ninius@online.no>
2024-12-05 07:27:45 +0100

    Day 5 Complete

--- src/05.py
+++ src/05.py
@@ -7,17 +7,143 @@ def test():
 
     input = [
 
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47"
+
     ]
 
     return input
 
+def parse(input):
+
+    rules = []
+    updates = []
+
+    for line in input:
+
+        if "|" in line:
+
+            rule = line.split("|")
+            X = int(rule[0])
+            Y = int(rule[1])
+            rules.append([X,Y])
+
+        elif "," in line:
+            order = line.split(",")
+            numbers = []
+            for number in order:
+                numbers.append(int(number))
+
+            updates.append(numbers)
+
+    return rules, updates
+
+def valid(rules, update):
+
+    for number in update:
+        for rule in rules:
+
+            if rule[0] == number:
+                if rule[1] in update:
+                    if update.index(number) > update.index(rule[1]):
+                        return False
+                
+            if rule[1] == number:
+                if rule[0] in update:
+                    if update.index(number) < update.index(rule[0]):
+                        return False
+                
+    return True
+
+def fix(rules, update):
+
+
+    while not valid(rules, update):
+
+        for number in update:
+            for rule in rules:
+
+                if rule[0] == number:
+                    if rule[1] in update:
+                        if update.index(number) > update.index(rule[1]):
+                            update.pop(update.index(number))
+                            update.insert(update.index(rule[1])-1, number)
+
+                if rule[1] == number:
+                    if rule[0] in update:
+                            if update.index(number) < update.index(rule[0]):
+                                update.pop(update.index(number))
+                                update.insert(update.index(rule[0])+1, number)
+
+    return update
+
+
+def calculate_valid(rules, updates):
+
+    total = 0
+
+    for update in updates:
+
+        if valid(rules, update):
+            total += update[len(update)//2]
+
+    return total
+
+def calculate_invalid(rules, updates):
+
+    total = 0
+
+    for update in updates:
+
+        if not valid(rules, update):
+            
+            fixed = fix(rules, update)
+
+            total += fixed[len(update)//2]
+
+    return total
+
 def part1(input):
 
-    print("Part 1: ")
+    rules, updates = parse(input)
+
+    total = calculate_valid(rules, updates)
+
+    print("Part 1: The total value is", total)
 
 def part2(input):
 
-    print("Part 2: ")
+    rules, updates = parse(input)
+
+    total = calculate_invalid(rules, updates)
+
+    print("Part 2: The total value is", total)
 
 filename = "../input/05.txt"
 input = files.input_as_list(filename)
